85ccea87101e9d783bd629c34c50528da429eff9
Godwin Adigun <adigungodwin2@gmail.com>
2022-03-15 22:08:21 +0100

    rectified some bugs

--- account/models.py
+++ account/models.py
@@ -1,66 +1,60 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
-# Create your models here.
+
 class MyAccountManager(BaseUserManager):
-    def create_user(self, email, username, first_name, last_name, password=None):
-        """Creates and saves a user with the given details"""
+    """Creates and saves a user with the given details"""
+    def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError("User must provide an email address")
+            raise ValueError("The user must provide an email")
         if not username:
-            raise ValueError("User must provide a username")
-        if not first_name:
-            raise ValueError("User must provide a firstname")
-        if not last_name:
-            raise ValueError("User must provide a lastname")
+            raise ValueError("The user must provide a username")
 
         user = self.model(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
+            email=self.normalize_email(email),
+            username=username,
         )
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, username, first_name, last_name, password):
+    def create_superuser(self, email, username, password):
         """Creates and saves a superuser with the given details"""
         user = self.create_user(
-            email = self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name,
-            password = password,
+            email=email,
+            username=username,
+            password=password
         )
 
-        user.is_staff = True
         user.is_admin = True
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
 
 class Account(AbstractBaseUser):
-    email           = models.EmailField(verbose_name='email address', max_length=50, unique=True)
-    username        = models.CharField(unique=True, max_length=25)
-    first_name      = models.CharField(max_length=30)
-    last_name       = models.CharField(max_length=30)
-    date_joined     = models.DateTimeField(verbose_name='date joined', auto_now_add=True)
-    last_login      = models.DateTimeField(verbose_name='last login', auto_now=True)
-    is_admin        = models.BooleanField(default=False)
-    is_active       = models.BooleanField(default=True)
-    is_staff        = models.BooleanField(default=False)
-    is_supersuer    = models.BooleanField(default=False)
+    """Creates the fields for the details in the database"""
+    email                   = models.EmailField(unique=True, max_length=50, verbose_name='email address')
+    username                = models.CharField(unique=True, max_length=25)
+    date_joined             = models.DateTimeField(auto_now_add=True)
+    last_login              = models.DateTimeField(auto_now=True)
+    is_admin                = models.BooleanField(default=False)
+    is_staff                = models.BooleanField(default=False)
+    is_active               = models.BooleanField(default=True)
+    is_superuser            = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+    REQUIRED_FIELDS = ['username', ]
 
     objects = MyAccountManager()
 
     def __str__(self):
         return f"Username: {self.username} Email: {self.email}"
 
+
     def has_perm(self, perm, obj=None):
+        """Checks if the user has any permissions"""
         return self.is_admin
 
     def has_module_perms(self, app_label):
+        """Checks if the user has permission to view the app 'app_label'"""
         return True
