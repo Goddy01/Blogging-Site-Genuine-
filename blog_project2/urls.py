3802318cdd2b3a2ff6e0d85357b9a976b01dafac
Godwin Adigun <adigungodwin2@gmail.com>
2022-03-22 16:23:27 +0100

    setup password reset functionality

--- blog_project2/urls.py
+++ blog_project2/urls.py
@@ -28,4 +28,30 @@ urlpatterns = [
     re_path(r'^logout/$', logout_view, name='logout'),
     re_path(r'^login/$', login_view, name='login'),
     re_path(r'^login/update_profile/$', update_view, name='update'),
-]
+
+
+    path(
+    "password_change/", views.PasswordChangeView.as_view(), name="password_change"
+            ),
+    path(
+        "password_change/done/",
+        views.PasswordChangeDoneView.as_view(),
+        name="password_change_done",
+            ),
+    path("password_reset/", views.PasswordResetView.as_view(), name="password_reset"),
+    path(
+        "password_reset/done/",
+        views.PasswordResetDoneView.as_view(),
+        name="password_reset_done",
+            ),
+    path(
+        "reset/<uidb64>/<token>/",
+        views.PasswordResetConfirmView.as_view(),
+        name="password_reset_confirm",
+            ),
+    path(
+        "reset/done/",
+        views.PasswordResetCompleteView.as_view(),
+        name="password_reset_complete",
+            ),
+    ]
