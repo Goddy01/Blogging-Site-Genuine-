59f4afc7bd67b0b5f314e427b1ef9c0b3087bbfe
Godwin Adigun <adigungodwin2@gmail.com>
2022-03-23 12:26:03 +0100

    updates blog.models

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.db.models.signals import pre_save
 from django.utils.text import slugify
 from django.conf import settings 
-from django.db.models.signals import post_delete
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 
 
@@ -12,11 +11,26 @@ def upload_location(instance, filename):
     return file_path
 
 class BlogPost(models.Model):
-    """Creates these fields in the database which will be the attributes of a post"""
-    title                   = models.Charield(max_length=50, blank=False, null=False)
-    body                    = models.TextField(maxx_length=5000, blank=False, null=False)
+    """Creates these fields in the database which will all be the attributes of a post"""
+    title                   = models.CharField(max_length=50, blank=False, null=False)
+    body                    = models.TextField(max_length=5000, blank=False, null=False)
     image                   = models.ImageField(upload_to=upload_location, blank=False, null=False)
     date_published          = models.DateTimeField(auto_now_add=True, verbose_name="date published")
-    date_updated            = models.DateTimeField(auto_add=True, verbose_name="date updated")
-    author                  = models.ForeignKey(settings.AUTH.UAER_MODEL, on_delete=models.CASCADE)
+    date_updated            = models.DateTimeField(auto_now=True, verbose_name="date updated")
+    author                  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     slug                    = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
+
+@receiver(post_delete, sender=BlogPost)
+def submission_delete(sender, instance, **kwargs):
+    """Deletes the image of a blog-post when the correlating BlogPost is deleted"""
+    instance.image.delete(False)
+
+
+def pre_save_blog_post_receiver(sender, instance, **kwargs):
+    """Checks if a blog post has a slug, if not it creates one. This executes before each blog post is commited to the database"""
+    if not instance.slug:
+        instance.slug = slugify(instance.author.username + "-" + instance.title)
+pre_save.connect(pre_save_blog_post_receiver, sender=BlogPost)
