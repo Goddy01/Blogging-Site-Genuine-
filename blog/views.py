429a5746e90854e7fa5f65d8ed8ad8e7abec2535
Godwin Adigun <adigungodwin2@gmail.com>
2022-03-26 15:26:30 +0100

    added the view to update a blog post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_object_or_404, render, redirect
-from .forms import CreateBlogPostForm
+from .forms import (CreateBlogPostForm,
+                    UpdateBlogPostForm
+                    )
 from account.models import Account
 from .models import BlogPost
 
@@ -9,6 +11,7 @@ from blog.models import (
 # Create your views here.
 
 def create_blog_view(request):
+    """The view to create a blog post"""
     if not request.user.is_authenticated:
         return redirect ('must_auth')
     context = {}
@@ -17,7 +20,7 @@ def create_blog_view(request):
     create_blog_form = CreateBlogPostForm(request.POST or None, request.FILES or None)
     if create_blog_form.is_valid():
         obj = create_blog_form.save(commit=False)
-        author = Account.objects.filter(email=user.email).first()
+        author = Account.objects.filter(email=user.email).first() # Return a queryset '.first. gets the first item there
         obj.author = author
         obj.save()
         create_blog_form = CreateBlogPostForm()
@@ -27,7 +30,30 @@ def create_blog_view(request):
 
 
 def blog_details_view(request, slug):
+    """The view to get the details of a blog"""
     context = {}
     blog_details = get_object_or_404(BlogPost, slug=slug)
     context['blog_details'] = blog_details
     return render(request, 'blog/blog_details.html', context)
+
+
+def update_blog_post_view(request, slug):
+    """The view to update a blog post"""
+    context = {}
+    user = request.user
+    if not user.is_authenticated:
+        redirect('must_auth')
+
+    blog_post = get_object_or_404(BlogPost, slug=slug)
+    if request.method == 'POST':
+        update_form = UpdateBlogPostForm(request.POST or None, request.FILES or None, instance=request.user)
+        if update_form.is_valid():
+            object = update_form.save()
+            context['success_message'] = 'Blog Post has been updated.'
+
+    else:
+        update_form = UpdateBlogPostForm(instance=request.user)
+
+    context['blog_post'] = blog_post
+    context['update_form'] = update_form
+    return render(request, 'blog/edit_blog_post.html', context)
