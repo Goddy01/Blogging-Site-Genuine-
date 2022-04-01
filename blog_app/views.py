543f9d79c233a0a7e1e403e3a4dfc31cd4fe55b8
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-01 15:57:32 +0100

    added the view to display blog posts on homepage

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,12 +1,9 @@
-import imp
 from django.shortcuts import render
-from account.models import Account
-
+from operator import attrgetter
+from blog.models import BlogPost
 # Create your views here.
 def home_page(request):
-    accounts = Account.objects.all()
-    context = {
-        'accounts': accounts
-    }
-
+    context = {}
+    blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True) # Sorts the blog post by their dates, here the latest/newest blog posts will comr first in the list.
+    context['blog_posts'] = blog_posts
     return render(request, 'blog_app/home.html', context)
