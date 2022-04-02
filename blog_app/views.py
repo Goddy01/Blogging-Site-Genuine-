935487a7d238be638bca09f2eed63f55625ea060
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-02 07:16:39 +0100

    added the search word to context

--- blog_app/views.py
+++ blog_app/views.py
@@ -4,6 +4,13 @@ from blog.models import BlogPost
 # Create your views here.
 def home_page(request):
     context = {}
+    
     blog_posts = sorted(BlogPost.objects.all(), key=attrgetter('date_updated'), reverse=True) # Sorts the blog post by their dates, here the latest/newest blog posts will comr first in the list.
+
+    queries = ""
+    if request.GET:
+        queries = request.GET
+    context['queries'] = queries
+
     context['blog_posts'] = blog_posts
     return render(request, 'blog_app/home.html', context)
