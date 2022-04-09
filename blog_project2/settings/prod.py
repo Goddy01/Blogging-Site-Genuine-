143f886827c6f786ec54e133236d4cc82f2e8d66
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-09 08:53:48 +0100

    testin

--- blog_project2/settings/prod.py
+++ blog_project2/settings/prod.py
@@ -83,5 +83,5 @@ LOGGING = {
 }
 
 # Heroku Settings
-django_on_heroku.settings(locals(), staticfiles=False)
+django_on_heroku.settings(locals(), static=False)
 del DATABASES['default']['OPTIONS']['sslmode']
