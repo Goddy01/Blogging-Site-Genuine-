f31261c19707dcc87a611be4f005325b1f4fd173
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-10 23:24:31 +0100

    added settings for password reset

--- blog_project2/settings/base.py
+++ blog_project2/settings/base.py
@@ -166,3 +166,12 @@ CKEDITOR_CONFIGS = {
 # config.width = '75%';   // CSS unit (percent).
 
 
+# SMTP CONFIGURATION
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLC = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
