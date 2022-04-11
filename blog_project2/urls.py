e686e3cd6a3f06fb0694d593475ef237b520dbf2
Godwin Adigun <adigungodwin2@gmail.com>
2022-04-11 15:30:51 +0100

    password reset

--- blog_project2/urls.py
+++ blog_project2/urls.py
@@ -48,7 +48,7 @@ urlpatterns = [
 
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_form.html'), name='password_reset_confirm'),
     
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset'),
     
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
      name='password_reset_complete'),
