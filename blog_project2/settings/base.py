b2da022cb7db88509000153c1a83b5a0132e97ba
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-27 11:51:29 +0100

    integration google__app_password

--- blog_project2/settings/base.py
+++ blog_project2/settings/base.py
@@ -173,8 +173,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'bloggie.blogapp@gmail.com'
+EMAIL_HOST_PASSWORD = 'bzimpndmxtubdpyy'
 # DEFAULT_FROM_EMAIL = 'Bloggie <noreply@bloggie-blogapp.com>'
 # SERVER_EMAIL = 'bloggie.blogapp@gmail.com'
 
