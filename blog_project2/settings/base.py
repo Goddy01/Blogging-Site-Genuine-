e9e8e6284110c8e9a9a53584f35ff69c5aae85bb
Godwin Adigun <adigungodwin2@gmail.com>
2022-08-27 11:29:21 +0100

    integration google__app_password

--- blog_project2/settings/base.py
+++ blog_project2/settings/base.py
@@ -175,8 +175,8 @@ EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-DEFAULT_FROM_EMAIL = 'Bloggie <noreply@bloggie-blogapp.com>'
-SERVER_EMAIL = 'bloggie.blogapp@gmail.com'
+# DEFAULT_FROM_EMAIL = 'Bloggie <noreply@bloggie-blogapp.com>'
+# SERVER_EMAIL = 'bloggie.blogapp@gmail.com'
 
 
 # SEN GRID
