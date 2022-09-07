3adcb30968a720170578e5914195b6d1fbfe84e4
Godwin Adigun <adigungodwin2@gmail.com>
2022-09-07 16:11:50 +0100

    fixing password reset

--- blog_project2/settings/base.py
+++ blog_project2/settings/base.py
@@ -171,10 +171,10 @@ if DEBUG:
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bloggie.blogapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'cnzfucrnutxtjjvj'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Bloggie <noreply@bloggie-blogapp.com>'
-# SERVER_EMAIL = os.environ['EMAIL_HOST_USER']
+SERVER_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # SEN GRID
